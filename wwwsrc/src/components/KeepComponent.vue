758f3db429e11fb83559d704c3f29b4f651e0078
micahjbartron <micahjbartron@gmail.com>
2020-07-20 21:09:12 -0600

    fix

--- wwwsrc/src/components/KeepComponent.vue
+++ wwwsrc/src/components/KeepComponent.vue
@@ -5,7 +5,14 @@
       <div class="card-body">
         <h4 class="card-title">{{keepProp.name}}</h4>
         <p class="card-text">{{keepProp.description}}</p>
-        <i class="fa fa-plus" aria-hidden="true"></i>
+        <div class="row">
+          <div class="col-6">
+            <i @click="addToVault" class="fa fa-plus" aria-hidden="true"></i>
+          </div>
+          <div class="col-6 justify-content end">
+            <button v-if="keepProp.id == $auth.user.sub" class="btn btn-outline-danger">delete</button>
+          </div>
+        </div>
       </div>
     </div>
   </div>
